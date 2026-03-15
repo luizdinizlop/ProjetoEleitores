114b8ea43cea1c6b6bbb1fb83e4fc82f07879469
luizdinizlop <luizdinizlop@gmail.com>
2026-03-15 11:53:49 -0300

    Add files via upload

---
+++ ProjetoEleitor/Program.cs
@@ -0,0 +1,11 @@
+﻿int idade;
+
+Console.WriteLine("Qual é a sua idade?");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 16)
+    Console.WriteLine("Você não é eleitor.");
+else if (idade >= 18 && idade <= 69)
+    Console.WriteLine("Você é um eleitor orbigatório");
+else
+    Console.WriteLine("Você é um eleitor facultativo.");
